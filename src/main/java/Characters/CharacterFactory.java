882aaca11608fd624b025909b771461a07445f5a
Vitalik <91615314+Vitalik001@users.noreply.github.com>
2022-10-11 16:02:27 +0300

    fixed maven

--- src/main/java/Characters/CharacterFactory.java
+++ src/main/java/Characters/CharacterFactory.java
@@ -4,6 +4,7 @@ import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class CharacterFactory {
 
@@ -11,7 +12,7 @@ public class CharacterFactory {
         Random rd = new Random();
         Reflections reflections = new Reflections("Characters");
         var type =
-                reflections.getSubTypesOf(Character.class).stream().toList();
+                reflections.getSubTypesOf(Character.class).stream().collect(Collectors.toList());
 //        System.out.println(type);
         int idx = rd.nextInt(type.size());
         return type.get(idx).getDeclaredConstructor().newInstance();
